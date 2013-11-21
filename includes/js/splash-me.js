28b767ad8d357833b343aca47a2608291d641273
unknown <Tom@Tom-PC.(none)>
2013-11-21 19:03:30 +0000

    Added aditional validation and fixed the URL redirect issue

--- includes/js/splash-me.js
+++ includes/js/splash-me.js
@@ -1,4 +1,4 @@
-function colorBoxOpen (jQuerySelector, ajaxUrl, modalWidth, modalClose) {
+function colorBoxOpen (jQuerySelector, ajaxUrl, modalWidth, overlayOpacity, overlayColour, modalClose, continueLinkClose) {
 
 	jQuery.ajax({
 
@@ -11,28 +11,58 @@ function colorBoxOpen (jQuerySelector, ajaxUrl, modalWidth, modalClose) {
 	        jQuery.colorbox({
 	            html: content,
 	            width: modalWidth,
-	            closeButton : modalClose,
-	            scrolling : false
+	            closeButton : false,
+	            scrolling : false,
+	            opacity : overlayOpacity,
+	            onOpen : function () {
+	            	if (!modalClose) {
+	            		jQuery('#cboxClose').remove();
+	            	} 
+	            	jQuery('#cboxOverlay').css({
+	            		background : overlayColour
+	            	});
+	            	
+	            },
+	            onComplete : function () {
+
+	            	jQuery.colorbox.resize();
+
+	            	var contCloseButton = jQuery('a#splash-me-close');
+
+	            	if (continueLinkClose == 1) {
+		            	contCloseButton.click( function (e) {
+							jQuery.colorbox.close();
+							e.preventDefault();
+						});
+	            	}
+
+	            }
 	        });
 
 	    } 
 
 	});
 
+
 }
+
+
 
 window.onload = function () {
 
 	var jQuerySelector = Drupal.settings.splashMe.jQuerySelector,
 		ajaxUrl = Drupal.settings.splashMe.ajaxUrl,
 		modalWidth = Drupal.settings.splashMe.modalWidth,
+		overlayOpacity = Drupal.settings.splashMe.overlayOpacity,
+		overlayColour = Drupal.settings.splashMe.overlayColour,
 		modalClose = Drupal.settings.splashMe.modalClose == 1 ? true :  false;
-		delayTime = Drupal.settings.splashMe.delayTime;
+		delayTime = Drupal.settings.splashMe.delayTime,
+		continueLinkClose = Drupal.settings.splashMe.continueLinkClose;
 
 	if (delayTime) {
-		setTimeout(function(){colorBoxOpen(jQuerySelector, ajaxUrl, modalWidth, modalClose)}, delayTime);
+		setTimeout(function(){colorBoxOpen(jQuerySelector, ajaxUrl, modalWidth, overlayOpacity, overlayColour, modalClose, continueLinkClose)}, delayTime);
 	} else {
-		colorBoxOpen(jQuerySelector, ajaxUrl, modalWidth, modalClose);
+		colorBoxOpen(jQuerySelector, ajaxUrl, modalWidth, overlayOpacity, overlayColour, modalClose, continueLinkClose);
 	}
 
 
